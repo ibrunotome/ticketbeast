29afe0e21b41656aac8a3db7efb2fa36ad6b4f4a
Bruno Tomé <ibrunotome@gmail.com>
2018-03-02 10:25:18 -0300

    PURCHASING CONCERT TICKETS - Getting Started with Validation Testing #15

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -12,10 +12,16 @@ class PurchaseTicketsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->paymentGateway = new FakePaymentGateway();
+        $this->app->instance(PaymentGateway::class, $this->paymentGateway);
+    }
+
     function testCustomerCanPurchaseConcertTickets()
     {
-        $paymentGateway = new FakePaymentGateway();
-        $this->app->instance(PaymentGateway::class, $paymentGateway);
 
         $concert = factory(Concert::class)->create([
             'ticket_price' => 3250
@@ -24,17 +30,32 @@ class PurchaseTicketsTest extends TestCase
         $this->json('POST', '/concerts/' . $concert->id . '/orders', [
             'email'           => 'john@example.com',
             'ticket_quantity' => 3,
-            'payment_token'   => $paymentGateway->getValidTestToken()
+            'payment_token'   => $this->paymentGateway->getValidTestToken()
         ]);
 
         // Assert
         $this->assertResponseStatus(201);
 
         // Make sure the customer was charged the correct amount
-        $this->assertEquals(9750, $paymentGateway->totalCharges());
+        $this->assertEquals(9750, $this->paymentGateway->totalCharges());
 
         // Make sure that an order exists for this customer
         $order = $concert->orders->where('email', 'john@example.com')->first();
         $this->assertNotNull($order);
-        $this->assertEquals(3, $order->tickets()->count());    }
+        $this->assertEquals(3, $order->tickets()->count());
+    }
+
+    public function testEmailIsRequiredToPurchaseTickets()
+    {
+        $concert = factory(Concert::class)->create();
+
+        $this->json('POST', '/concerts/' . $concert->id . '/orders', [
+            'ticket_quantity' => 3,
+            'payment_token'   => $this->paymentGateway->getValidTestToken()
+        ]);
+
+        $this->assertResponseStatus(422);
+
+        $this->assertArrayHasKey('errors', $this->decodeResponseJson());
+    }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,9 @@
 
 namespace Tests;
 
+use App\Exceptions\Handler;
+use Exception;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use Laravel\BrowserKitTesting\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -9,4 +12,23 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication;
 
     public $baseUrl = 'http://localhost:8000';
+
+    protected function disableExceptionHandling()
+    {
+        $this->app->instance(ExceptionHandler::class, new class extends Handler
+        {
+            public function __construct()
+            {
+            }
+
+            public function report(Exception $e)
+            {
+            }
+
+            public function render($request, Exception $e)
+            {
+                throw $e;
+            }
+        });
+    }
 }
