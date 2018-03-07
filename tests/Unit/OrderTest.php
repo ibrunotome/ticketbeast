feb3488c7f7ec5ec9f4f9658fba46df090ee07d4
Bruno Tomé <ibrunotome@gmail.com>
2018-03-07 15:33:48 -0300

    LIMITING TICKETS SALES - Cancelling Failed Orders #23

---
+++ tests/Unit/OrderTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Concert;
+use App\Models\Order;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class OrderTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testTicketsAreReleasedWhenAnOrderIsCanceled()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->addTickets(10);
+        $order = $concert->orderTickets('jane@example', 5);
+        $this->assertEquals(5, $concert->ticketsRemaining());
+
+        $order->cancel();
+
+        $this->assertEquals(10, $concert->ticketsRemaining());
+
+        $this->assertNull(Order::find($order->id));
+    }
+}
